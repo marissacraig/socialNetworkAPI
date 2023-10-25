90148855f3805b499b6b315cd83d9d2cf2c914a7
marissacraig <gersen.marissa@gmail.com>
2023-10-25 15:04:44 -0700

    created friend routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,15 +74,20 @@ addFriend(req, res) {
     )
     .catch((err) => res.status(500).json(err));
   },
-// Delete a friend
-deleteFriend(req, res) {
-  User.findOneAndDelete({ _id: req.params.userId })
-    .then((user) =>
-      !user
-        ? res.status(404).json({ message: 'No user with that ID' })
-        : Thought.deleteMany({ _id: { $in: user.thoughts } })
+  // Remove friend from a user
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friend: { friendId: req.params.friendId } } },
+      { runValidators: true, new: true }
     )
-    .then(() => res.json({ message: 'User and thoughts deleted!' }))
-    .catch((err) => res.status(500).json(err));
-}
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
